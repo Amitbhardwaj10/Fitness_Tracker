4f214ca2008e2458af699d06595e216059640294
Amit Bhardwaj <amitbhk2002@gmail.com>
2024-08-22 02:35:07 +0530

    changed the routing methods

--- src/main.jsx
+++ src/main.jsx
@@ -1,18 +1,20 @@
 import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { Route, RouterProvider, createBrowserRouter, createRoutesFromElements } from "react-router-dom";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
 import Contact from "./pages/Contact";
+const router = createBrowserRouter(
+	createRoutesFromElements(
+		<Route path="/" element={<App/>} >
+		<Route path="" element={<Home />} />
+		<Route path="/contact" element={<Contact />} />
+		<Route path="/login" element={<Login />} />
+	</Route>
+	)
+);
 
 createRoot(document.getElementById("root")).render(
-	<BrowserRouter>
-		<App />
-		<Routes>
-			<Route path="/" element={<Home />} />
-			<Route path="/contact" element={<Contact />} />
-			<Route path="/login" element={<Login />} />
-		</Routes>
-	</BrowserRouter>
+	<RouterProvider router={router} />
 );
