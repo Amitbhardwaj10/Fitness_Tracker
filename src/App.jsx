72001176fc094beaf92de5f1a5edfb7e15c98281
Amit Bhardwaj <amitbhk2002@gmail.com>
2024-08-22 02:09:32 +0530

    shifted the routing from app.jsx to main.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,10 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
 import Header from "./components/Header";
-import Home from "./pages/Home";
-import Login from "./pages/Login";
-import Contact from "./pages/Contact";
 
 function App() {
 	return (
 		<>
-			<BrowserRouter>
-				<Header />
-				<Routes>
-					<Route path="/" element={<Home />} />
-					<Route path="/contact" element={<Contact />} />
-					<Route path="/login" element={<Login />} />
-				</Routes>
-			</BrowserRouter>
+			<Header />
 		</>
 	);
 }
--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,18 @@
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import { createRoot } from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Contact from "./pages/Contact";
 
-createRoot(document.getElementById('root')).render(
-  <App />
-)
+createRoot(document.getElementById("root")).render(
+	<BrowserRouter>
+		<App />
+		<Routes>
+			<Route path="/" element={<Home />} />
+			<Route path="/contact" element={<Contact />} />
+			<Route path="/login" element={<Login />} />
+		</Routes>
+	</BrowserRouter>
+);
